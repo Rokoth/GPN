a2561e8f3ed60c471f4398fa370f72e56db4ddb5
Dmitrii Milyaev <dmitriy.milyaev@outlook.com>
2024-10-16 18:03:52 +0500

    [Feature-0.0.1] Добавил модель БД Person

---
+++ App/GPN/GPN.DB/Model/Person.cs
@@ -0,0 +1,22 @@
+﻿using GPN.DB.Attributes;
+
+namespace GPN.DB.Model
+{
+    [TableName("person")]
+    public class Person : Entity
+    {
+        [ColumnName("name")]
+        public string Name { get; set; }
+        [ColumnName("description")]
+        public string Description { get; set; }
+
+        [ColumnName("user_id")]
+        public string UserId { get; set; }
+
+        [ColumnName("point_id")]
+        public string PointId { get; set; }
+
+        [ColumnName("last_enter_date")]
+        public DateTimeOffset LastEnterDate { get; set; }
+    }
+}
---
+++ App/GPN/GPN.DB/Model/PersonHistory.cs
@@ -0,0 +1,22 @@
+﻿using GPN.DB.Attributes;
+
+namespace GPN.DB.Model
+{
+    [TableName("h_person")]
+    public class PersonHistory : EntityHistory
+    {
+        [ColumnName("name")]
+        public string Name { get; set; }
+        [ColumnName("description")]
+        public string Description { get; set; }
+
+        [ColumnName("user_id")]
+        public string UserId { get; set; }
+
+        [ColumnName("point_id")]
+        public string PointId { get; set; }
+
+        [ColumnName("last_enter_date")]
+        public DateTimeOffset LastEnterDate { get; set; }
+    }
+}
